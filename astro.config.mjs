dc2d1153792f390a2050c7af825a1e82bef146dc
SwissEliteVan <214782361+SwissEliteVan@users.noreply.github.com>
2026-08-03 09:55:23 +0200

    mise à jour

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://clicom.ch',
+  integrations: [sitemap({ filter: (page) => page !== 'https://clicom.ch/404/' })],
   output: 'server',
   adapter: node({
     mode: 'standalone',
